59b605f95794638af081c5b70d5a987e007bbcf0
Seyi Onifade <xyluz@ymail.com>
2017-10-21 06:03:37 +0100

    auth done

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,8 +13,9 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
-        //
+    { 
+           //fixing db migration errors
+           Schema::defaultStringLength(191); 
     }
 
     /**
