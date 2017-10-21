f4ba914a8871b87d68847eec78282999bac5e4ba
Seyi Onifade <xyluz@ymail.com>
2017-10-21 22:51:33 +0100

    badges model

--- app/Badges.php
+++ app/Badges.php
@@ -2,9 +2,18 @@
 
 namespace App;
 
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 
 class Badges extends Model
 {
-    //
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'icon', 'points','description'
+    ];
 }
