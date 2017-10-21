ef20c95bdc16dee1446848c25db02bf43667c8d0
Seyi Onifade <xyluz@ymail.com>
2017-10-21 22:57:33 +0100

    player badge model

--- app/PlayerBadges.php
+++ app/PlayerBadges.php
@@ -2,9 +2,17 @@
 
 namespace App;
 
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 
 class PlayerBadges extends Model
 {
-    //
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'player', 'badge'
+    ];
 }
