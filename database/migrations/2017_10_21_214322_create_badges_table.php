8d98c3feaa3f88da1b4e91131be00e9993f93e1d
Seyi Onifade <xyluz@ymail.com>
2017-10-21 22:50:39 +0100

    badges migration

--- database/migrations/2017_10_21_214322_create_badges_table.php
+++ database/migrations/2017_10_21_214322_create_badges_table.php
@@ -14,6 +14,9 @@ class CreateBadgesTable extends Migration
     public function up()
     {
         Schema::create('badges', function (Blueprint $table) {
+            $table->string('name'); //name of this badge
+            $table->string('icon'); //badge icon
+            $table->integer('points'); // points associated with this badge
             $table->increments('id');
             $table->timestamps();
         });
