30e5b945712c22cf200da05a1d34bbc4e4ac4e74
Seyi Onifade <xyluz@ymail.com>
2017-10-24 08:25:07 +0100

    install tracy for better debugging

--- config/app.php
+++ config/app.php
@@ -163,7 +163,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        Recca0120\LaravelTracy\LaravelTracyServiceProvider::class,
+        
         /*
          * Package Service Providers...
          */
