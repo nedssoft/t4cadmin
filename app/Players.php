69c4acf0ac01208c5b96ba3364714c279489c805
Seyi Onifade <xyluz@ymail.com>
2017-10-24 08:14:33 +0100

    player details 90%

--- app/Players.php
+++ app/Players.php
@@ -13,7 +13,24 @@ class Players extends Model
      */
     protected $fillable = [
         'name', 'email', 'password','phone','username'
-    ];
+    ]; 
 
+    //get player badges
+
+    public function badges(){
+    	return $this->hasMany('App\Badges');
+    }
+
+    //get player levels
+
+    public function level(){
+        return $this->hasOne('App\Levels');
+    }
     
+    //get player points and money
+
+    public function point(){
+        return $this->hasOne('App\Points');
+    }
+
 }
