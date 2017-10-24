a861ac54b60c4b3f87b986b1fbc8b251f186ae33
Seyi Onifade <xyluz@ymail.com>
2017-10-24 15:40:18 +0100

    menu and view adjustment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading"> Welcome, {{Auth::user()->name}} </div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -14,7 +14,16 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <div class="alert alert-default alert-dismissable fade in">
+                    <p><strong>Menu</strong></p><br>
+                    <a href="/categories" class="btn btn-default">Category</a>
+                    <a href="/contact" class="btn btn-default">Questions</a>
+                    <a href="/account" class="btn btn-default">Badges</a>                   
+                    <a href="/help" target="_blank" class="btn btn-default">Levels</a>
+                    <a href="/help" target="_blank" class="btn btn-default">Points</a>
+                    <a href="/help" target="_blank" class="btn btn-default">Admin</a>
+                  </div>
+                   
                 </div>
             </div>
         </div>
